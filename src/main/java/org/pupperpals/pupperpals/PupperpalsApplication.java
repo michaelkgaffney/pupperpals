6b5ec5630d1d9a51728e0ebcabfb439fe8c12706
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-07-02 14:01:14 -0500

    initial commit

---
+++ src/main/java/org/pupperpals/pupperpals/PupperpalsApplication.java
@@ -0,0 +1,13 @@
+package org.pupperpals.pupperpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupperpalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupperpalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pupperpals/pupperpals/PupperpalsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pupperpals.pupperpals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupperpalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
