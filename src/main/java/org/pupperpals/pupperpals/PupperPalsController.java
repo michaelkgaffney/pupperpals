6f1efac6817e3a62053622ca94ad6542251555b9
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-07-02 16:35:44 -0500

    first endpoint with restdocs

---
+++ src/main/java/org/pupperpals/pupperpals/PupperPalsController.java
@@ -0,0 +1,16 @@
+package org.pupperpals.pupperpals;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class PupperPalsController {
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.FOUND)
+    public void redirectToDocs(HttpServletResponse httpServletResponse){
+        httpServletResponse.setHeader("Location", "/docs/index.html");
+    }
+}
