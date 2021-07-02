63e106e1e675cb3123df05c945d470bc21b106d2
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-07-02 16:37:57 -0500

    first endpoint with restdocs

--- src/main/java/org/pupperpals/pupperpals/PupperPalsController.java
+++ src/main/java/org/pupperpals/pupperpals/PupperPalsController.java
@@ -3,6 +3,7 @@ package org.pupperpals.pupperpals;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
